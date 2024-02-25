b8431ba019fda8443ce2f7723b7fac307259780d
noxi515 <tzo.ppl@gmail.com>
2024-02-26 02:05:45 +0900

    プロジェクト ファイルを追加します。

---
+++ src/SampleLib/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace SampleLib;
+
+// All the code in this file is included in all platforms.
+public class Class1
+{
+}
---
+++ src/SampleLib/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,6 @@
+﻿namespace SampleLib;
+
+// All the code in this file is only included on Android.
+public class PlatformClass1
+{
+}
